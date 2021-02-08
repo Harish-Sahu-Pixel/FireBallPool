6bd883eb3a941f1d30d43efb51f22e058da0a3d9
Harish Kumar Sahu <harishkumarsahu0806@gmail.com>
2021-02-08 21:31:48 +0530

    Update Coin.cs

--- Coin.cs
+++ Coin.cs
@@ -10,7 +10,7 @@ public class Coin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // pass
     }
 
     // Update is called once per frame
